ab1ee72c1a6c823fbfe996077866be9cd1a913af
Cedricaf <34737@ma-web.nl>
2023-05-11 14:40:49 +0200

    added files

---
+++ structuur/public/superglobals.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="afhandeling.php" method="get">
+    
+    <div>
+        <p><input type="text" name="name"></p>
+        <p><input type="email" name="email" /></p>
+
+    </div>
+    <button type="submit">Verzenden</button>
+</form>
+    
+    
+</body>
+</html>
